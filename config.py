10aa81340d633a29c3436c39f1943b52f3dbc8d4
oyethomassir <157202299+oyethomassir@users.noreply.github.com>
2024-01-25 12:18:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ API_ID = "6435225"
 API_HASH = "4e984ea35f854762dcde906dce426c2d"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
-MONGO_URL = getenv("MONGO_URL", None)
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://teamdaxx123:teamdaxx123@cluster0.ysbpgcp.mongodb.net/?retryWrites=true&w=majority")
 OWNER_ID = "6428719458"
 SUPPORT_GRP = "II_4ST_BALE_II"
 UPDATE_CHNL = "I_M_FIGHTER"
